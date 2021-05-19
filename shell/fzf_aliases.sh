fe22e492819893173a0b59eb099a4be177dba2a4
Anirudh Sundar <anirudh.sundar@gmail.com>
2021-05-19 10:34:30 +0530

    add "cdf" cmd - switch to directory containing file

--- shell/fzf_aliases.sh
+++ shell/fzf_aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export FZF_CTRL_T_OPTS="--height 80% --bind '?:toggle-preview' --preview '(highlight -O ansi -l {} 2> /dev/null || bat --style=numbers --color=always --line-range :500 {} || tree -C {}) 2> /dev/null | head -200'"
 # cdf - cd into the directory of the selected file
-cdf() {
+cdfz() {
    local file
    local dir
    file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
