8ad68977037775f7dbb65a5588db93e08481cce8
Anirudh Sundar <anirudh.sundar@gmail.com>
2022-01-12 22:26:21 +0530

    add manpages tmux binding and other small changes

--- shell/fzf_aliases.sh
+++ shell/fzf_aliases.sh
@@ -6,3 +6,14 @@ cdfz() {
    local dir
    file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
 }
+
+function man(){
+	MAN="/usr/bin/man"
+	if [ -n "$1" ]; then
+		$MAN "$@"
+		return $?
+	else
+		$MAN -k . | fzf --reverse --preview="echo {1,2} | sed 's/ (/./' | sed -E 's/\)\s*$//' | xargs $MAN" | awk '{print $1 "." $2}' | tr -d '()' | xargs -r $MAN
+		return $?
+	fi
+}
