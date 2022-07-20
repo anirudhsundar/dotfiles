f59d76b30f03aa5543cac4fe1b8ea9e6de65d03f
Anirudh Sundar <anirudh.sundar@gmail.com>
2022-07-20 11:50:44 +0530

    Add pathunique function to remove duplicates
    Example usage:
    pathunique PATH
    pathunique LD_LIBRARY_PATH
    pathunique PYTHONPATH

--- bash/custom.bash
+++ bash/custom.bash
@@ -7,3 +7,9 @@ shopt -s autocd
 export HISTSIZE=100000
 export HISTFILESIZE=100000
 export HISTCONTROL=erasedups
+
+function pathunique(){
+  p="$1"
+  echo ${p}=$(printf "%s" "${!p}" | awk -v RS=':' '!a[$1]++ { if (NR > 1) printf RS; printf $1 }')
+  export ${p}=$(printf "%s" "${!p}" | awk -v RS=':' '!a[$1]++ { if (NR > 1) printf RS; printf $1 }')
+}
