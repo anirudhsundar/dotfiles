39b06b9bda44bc2b17352353bd25c4ee3d6208a5
Anirudh Sundar <anirudh.sundar@gmail.com>
2023-11-12 10:52:27 +0530

    Set default editor to nvim when available

--- shell/common_paths.sh
+++ shell/common_paths.sh
@@ -1,5 +1,15 @@
 export PATH=$PATH:$HOME/.bin
 # export MANPAGER="vim -M +MANPAGER -"
 export MANPAGER=less
-export EDITOR="vim"
+
+if command -v nvim &> /dev/null
+then
+  export EDITOR="nvim"
+  export VISUAL="nvim"
+  export GIT_EDITOR="nvim"
+else
+  export EDITOR="vim"
+  export VISUAL="vim"
+  export GIT_EDITOR="vim"
+fi
 # export MANPATH=$MANPATH:$HOME/.man
